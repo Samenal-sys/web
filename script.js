41b9812dc061baaaf2cd7743ec5d8498f6dcb7de
Samenal-sys <samenaal@proton.me>
2025-02-27 22:15:03 +0000

    update

--- script.js
+++
@@ -1,9 +0,0 @@
-// script.js
-window.addEventListener('load', () => {
-    const loadingOverlay = document.getElementById('loading-overlay');
-
-    // Add a 2-second delay
-    setTimeout(() => {
-        loadingOverlay.classList.add('hidden');
-    }, 2000); // 2000 milliseconds = 2 seconds
-});
